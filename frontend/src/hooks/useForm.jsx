53f9facf59b99148bd51fd1a36fd72907c39c25a
Ekaterina Soloveva <KkatSsol@yandex.ru>
2025-03-17 17:36:10 +0000

    validateRules are placed in a separate file

--- frontend/src/hooks/useForm.jsx
+++ frontend/src/hooks/useForm.jsx
@@ -1,22 +1,5 @@
 import { useState } from "react";
-
-const validateRules = {
-  email: (value) => {
-    if (!value.trim()) return "Email обязателен!";
-    if (!/\S+@\S+\.\S+/.test(value)) return "Некорректный формат email!";
-    return null;
-  },
-  password: (value) => {
-    if (!value) return "Пароль обязателен!";
-    if (value.length < 6) return "Пароль должен быть не менее 6 символов!";
-    return null;
-  },
-  username: (value) => {
-    if (!value) return "Login обязателен!";
-    if (value.length < 3) return "Login должен быть не менее 3 символов!";
-    return null;
-  },
-};
+import validateRules from "../utils /validateRules";
 
 const useForm = (initialValues) => {
   const [formData, setFormData] = useState(initialValues);
