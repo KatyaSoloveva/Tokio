22e759f3ba7e57e0bcbc6463475e4243a0fa2701
Ekaterina Soloveva <KkatSsol@yandex.ru>
2025-03-21 21:03:08 +0000

    The layout of the About page has been created

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,9 +1,31 @@
+import styles from "./About.module.css";
+import Container from "../../components/Container/Container";
 import Main from "../../components/Main/Main";
 
 const About = () => {
   return (
-    <Main>
-      <h3>ghigiu</h3>
+    <Main className={styles.mainAbout}> 
+      <Container className={styles.mainContainer}>
+        <Container className={styles.container}>
+          <h2 className={styles.info}>What it is (insert description)</h2>
+          <p className={styles.aboutinfo}>information</p>
+        </Container>
+        <Container className={styles.container}>
+          <h2 className={styles.info}>Стек технологий</h2>
+          <ul class={styles.list}>
+            <li>Заполнить</li>
+            <li>Заполнить</li>
+            <li>Заполнить</li>
+          </ul>
+        </Container>
+        <Container className={styles.container}>
+          <p className={styles.info}>
+            Ссылка на github автора -{" "}
+            <a href="https://github.com/KatyaSoloveva">Github</a>
+          </p>
+        </Container>
+      </Container>
+      <Container className={styles.sideContainer}></Container>
     </Main>
   );
 };
