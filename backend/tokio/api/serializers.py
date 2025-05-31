c8993951338350b8d737a87aeb31db2cf37be31f
Ekaterina Soloveva <KkatSsol@yandex.ru>
2025-05-31 07:52:20 +0000

    is_system field has been added to read_only_fields in CategorySerializer

--- backend/tokio/api/serializers.py
+++ backend/tokio/api/serializers.py
@@ -36,6 +36,7 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+        read_only_fields = ('is_system',)
 
 
 class TaskWriteSerializer(serializers.ModelSerializer):
