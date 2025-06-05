db54e13ae54cafe0a484099cbed7f74b9854d51f
Ekaterina Soloveva <KkatSsol@yandex.ru>
2025-06-05 17:17:44 +0000

    nginx has been added to the project (images don't load on the site if you run them in containers)

--- backend/tokio/tokio/settings.py
+++ backend/tokio/tokio/settings.py
@@ -124,6 +124,8 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
